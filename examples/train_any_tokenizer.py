7eefd09d6c4ee6619050361480b26fe46fa4901e
ZurabDz <zuradzindzibadze06@gmail.com>
2023-02-25 21:14:35 +0400

    fixing dataset issue

--- examples/train_any_tokenizer.py
+++ examples/train_any_tokenizer.py
@@ -4,9 +4,10 @@ from language_engine.tokenizer.train_tokenizer import construct_tokenizer
 
 
 # TODO: fix changing directory warning
+# TODO: add on disk data loading
 @hydra.main(config_path="../language_engine/configs", config_name="tokenizer", version_base="1.1")
 def train_tokenizers(cfg):
-    data = load_dataset('text', cfg.dataset_name)
+    data = load_dataset(cfg.dataset_name)
     tokenizer = construct_tokenizer(data['train'], cfg, path=None)
     tokenizer.save_pretrained('tokenizer')
 
