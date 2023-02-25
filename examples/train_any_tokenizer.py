15b13efc9f3ab1aa1463f6adf8402fa667d7620d
ZurabDz <zuradzindzibadze06@gmail.com>
2023-02-25 17:28:47 +0400

    calling function

--- examples/train_any_tokenizer.py
+++ examples/train_any_tokenizer.py
@@ -3,8 +3,11 @@ import hydra
 from language_engine.tokenizer.train_tokenizer import construct_tokenizer
 
 
-@hydra.main(config_path="language_engine/language_engine/configs", config_name="tokenizer")
+# TODO: fix changing directory warning
+@hydra.main(config_path="../language_engine/configs", config_name="tokenizer", version_base="1.1")
 def train_tokenizers(cfg):
     data = load_dataset('text', data_files=['/home/penguin/GeorgianWritingWizard/data/data.txt'])
     tokenizer = construct_tokenizer(data['train'], cfg, path=None)
     tokenizer.save_pretrained('tokenizer')
+
+train_tokenizers()
