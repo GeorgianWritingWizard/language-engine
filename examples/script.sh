4df44deda5b9fbf23c89e8a7ef72084e54cce7ac
ZurabDz <zuradzindzibadze06@gmail.com>
2023-02-26 18:39:12 +0400

    from config

--- examples/script.sh
+++ examples/script.sh
@@ -3,4 +3,4 @@ python  train_lm.py --evaluation_strategy steps --per_device_train_batch_size 24
          --save_strategy steps --save_steps 100 --eval_steps 100 --logging_steps 100 \
          --dataloader_num_workers 2 --torch_compile True --output_dir ./out \
          --push_to_hub True --hub_model_id ZurabDz/albert-geo --hub_token <mytoken> \
-         --jit_mode_eval True -- learning_rate 5e-5
+         --jit_mode_eval True --learning_rate 5e-5
