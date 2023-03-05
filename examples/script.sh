94cb88f3dd04ce210d31b8951a034f712be21777
ZurabDz <zuradzindzibadze06@gmail.com>
2023-03-05 12:54:55 +0400

    changing up a script

--- examples/script.sh
+++ examples/script.sh
@@ -2,5 +2,8 @@ python  train_lm.py --evaluation_strategy steps --per_device_train_batch_size 24
          --per_device_eval_batch_size 24 --gradient_accumulation_steps 4 --eval_accumulation_steps 4 \
          --save_strategy steps --save_steps 100 --eval_steps 100 --logging_steps 100 \
          --dataloader_num_workers 2 --torch_compile True --output_dir ./out \
-         --push_to_hub True --hub_model_id ZurabDz/albert-geo --hub_token <mytoken> \
-         --jit_mode_eval True --learning_rate 5e-5 --warmup_ratio 0.1
+         --jit_mode_eval True --learning_rate 2e-5 --warmup_ratio 0.1 \
+         --dataset_name ZurabDz/geo_small_corpus_dedublicated_trash_off_tokenized \
+         --test_size 0.05 --tokenizer_name ZurabDz/GeoSentencePieceBPE_32768_v2 \
+         --model_name albert --from_scratch --fp16 True \
+         --push_to_hub True --hub_model_id ZurabDz/albert-geo --hub_token <mytoken>
