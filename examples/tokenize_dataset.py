ff501f178579fc4e60dcee280a7b36d559451912
ZurabDz <zuradzindzibadze06@gmail.com>
2023-03-09 23:59:17 +0400

    small tmp changes

--- examples/tokenize_dataset.py
+++ examples/tokenize_dataset.py
@@ -5,14 +5,15 @@ especially when experimenting, this script will preprocess it and make it `ready
 
 from language_engine.dataset.preprocess import huggingface_preprocessing
 from datasets import load_dataset
-from transformers import PreTrainedTokenizerFast
+from transformers import AutoTokenizer
 import os
 
 NUM_THREADS = os.cpu_count()
 
 #TODO :Make This script
-tokenizer = PreTrainedTokenizerFast.from_pretrained('ZurabDz/GeoSentencePieceBPE_32768_v2')
-dataset = load_dataset('ZurabDz/geo_small_corpus_dedublicated_trash_off')
+tokenizer = AutoTokenizer.from_pretrained('/home/penguin/GeorgianWritingWizard/language_engine/examples/outputs/2023-03-09/22-36-38/tokenizer')
+dataset = load_dataset('text', data_files={'train': ['/home/penguin/GeorgianWritingWizard/data/test_data.txt']})
+# dataset = load_dataset('ZurabDz/geo_small_corpus')
 
-tokenized_dataset = huggingface_preprocessing(dataset['train'], tokenizer, NUM_THREADS)
+tokenized_dataset = huggingface_preprocessing(dataset['train'], tokenizer, NUM_THREADS, max_seq_length=128)
 tokenized_dataset.save_to_disk('./processed_data')
